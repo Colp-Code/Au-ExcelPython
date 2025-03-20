9df40548b9aaac0e02078e52d1a742fba8d0a332
Colp <mononeitor300@gmail.com>
2025-03-19 19:46:11 -0500

    0.2
    -Sistema de registro

--- prac3.py
+++ prac3.py
@@ -9,11 +9,32 @@ except:
 
 linea = hoja.max_row + 1
 print(linea)
+hoja["A1"] = "Fecha"
+hoja["B1"] = "Descripcion"
+hoja["C1"] = "Responsable"
+hoja["D1"] = "Peso"
+hoja["E1"] = "Valor"
+hoja["F1"] = "Fuente"
 
-entrada = int(input(f'A que horas llegaste? '))
-salida = int(input(f'A que horas te fuiste? '))
+fecha = str(input("Fecha del registro: "))
+descripcion = str(input("Descripcion del registro: "))
+responsable = str(input("Responsable de este registro: "))
+peso = float(input("Peso del producto(Si es que tiene): "))
+valor = int(input("Valor del producto: "))
+fuente = str(input(f'¿De donde salio la plata?: '))
+print(f"""Los tados agregados fueron
+    La fecha es: {fecha}
+    La descripcion es: {descripcion}
+    El responsable es: {responsable}S
+    El peso es: {peso}
+    El valor es: {valor}
+    La fuente es: {fuente}
+"""
+      )
+#entrada = int(input(f'A que horas llegaste? '))
+#salida = int(input(f'A que horas te fuiste? '))
 
-hoja.append([linea - 1, entrada, salida,])
+hoja.append([fecha, descripcion, responsable, peso, valor, fuente,])
 
 wb.save("empleados.xlsx")
 print("listo")
