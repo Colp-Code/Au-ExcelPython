07fa9ea5d55e4df8226aa79a05919be3d8078c68
Colp <mononeitor300@gmail.com>
2025-03-26 20:54:41 -0500

    1.1.1
    No se si esta roto pero lo subo

--- prac3.py
+++ prac3.py
@@ -10,7 +10,6 @@ except:
     hoja = wb.active
 
 linea = hoja.max_row + 1
-print(linea)
 hoja["A1"] = "Fecha"
 hoja["B1"] = "Descripcion"
 hoja["C1"] = "Responsable"
@@ -20,91 +19,119 @@ hoja["F1"] = "Tipo de salida"
 
 current_date = dt.date.today()
 format_date = current_date.strftime("%d-%m-%y")
+ejemplo_data = current_date.strftime("%d%m%y")
 
 infoDescripcion = ["Aguacate hass","Aguacate papelito","Bold","Banano","Durazno","Limion Comun","Limon mandarino","Limon tahiti","Mandarina","Mango tomy","Maracuya","Naranja tangelo","Naranja valencia","Panela","Platano","Platano maduro","Guanabana","Almuerzo-Desayuno","Arriendo casa","Arriedo local","Arriendo parqueadero","BCS carro","Chevyplan","Entrada abastos","Gasolina","Gasto personal","Insumo","Lavado","Mario","Onces","Prestamo","Parqueadero carro","Parqueadero moto","Self security","Servicio de energia","Servicio de agua","Venta caja","Venta nequi","Venta daviplata"]
 infoNombres = ["Andres","Armando","Camilo P","Chalo","Dairo","Emilio","Hugo","Jairo","Jeisson","Jimmy","Jose Angel","Julian","Mario","Mayerly","Nacho","Robin","Sara","Sra Luz","Venta","Yudi"]
 tipoSalida = ["Abono","Adelanto de sueldo","Arriendo","Caja menor","Deuda","Flete","Gasto alimentacion","Gasto carro","Gasto moto","Insumo","Inversion","Servicion publicos","Sueldo","Venta"]
-try:
-    respuesta = int(input(f"""¿Quieres la fecha outomatica o la quieres poner?
-                          
-Poner = 1
-automatica = 2
-Cual es tu opcion: """))
-    if respuesta == 1:
-        fecha = str(input("Fecha del registro: "))
-    else:
-        fecha = format_date
-    
+while True:
+    try:
+        respuesta = int(input(f"""¿Quieres la fecha automatica o la quieres poner?
+                                                    
+    Poner = 1
+    automatica = 2
+    Cual es tu opcion: """))
+        if respuesta == 1:
+            fecha = str(input(f"""                       
+Acuerda que el formato tiene que ser dia/mes/año
+    Ejemplo {ejemplo_data}
+Fecha del registro: """))
+            errorTexto = int(fecha)
+            fechaFinal = "/".join([fecha[i:i+2] for i in range(0,len(fecha),2)])
+            print(fechaFinal[0:8])
+            break
+        else:
+            fecha = format_date
+            break
+    except:
+        print("Tipo de dato incorrecto intentalo de nuevo :)")
+        sys.exit(1)
+
+while True:
     try:
         print(f"""
-              ===================
-              Que tipo de descripcion desea?
-              ===================
-              """)
+                ===================
+                Que tipo de descripcion desea?
+                ===================
+                """)
         for i,x in enumerate(infoDescripcion):
             print(i + 1 ,x)
-        descripcion = int(input("Que opcion escojes: "))
-        indexDescripcion = infoDescripcion[descripcion - 1]
-        print(indexDescripcion)
+            descripcion = int(input("Que opcion escojes: "))
+            indexDescripcion = infoDescripcion[descripcion - 1]
+            print(indexDescripcion)
+        break
     except:
         print("Algo salio mal")
         sys.exit(1)
+
+while True:
     try:
         print(f"""
-              ===================
-              Quien fue el responsable?
-              ===================
-              """)
+                ===================
+                Quien fue el responsable?
+                ===================
+                """)
         for i, x in enumerate(infoNombres):
             print(i + 1 ,x)
-        responsable = int(input("Responsable de este registro: "))
-        indexNombre = infoNombres[responsable - 1]
-        print(indexNombre)
+            responsable = int(input("Responsable de este registro: "))
+            indexNombre = infoNombres[responsable - 1]
+            print(indexNombre)
+        break
     except:
-        print("Algo salio mal")
-        sys.exit(1)
+            print("Algo salio mal")
+            sys.exit(1)
+while True:
     try:
         print("""
-              ===================
-              Peso
-              ===================
-              """)
+                ===================
+                Peso
+                ===================
+                """)
         peso = float(input("Peso del producto(Si no tiene dale al enter): "))
+        break
     except:
         peso = 0
         print(f'El peso se escribira como 0')
+        break
+while True:
     try:
         print("""
-              ===================
-              Valor
-              ===================
-              """)
+                ===================
+                Valor
+                ===================
+                """)
         valor = float(input("Valor del producto(Si no tiene dale al enter): "))
+        break
     except:
         valor = 0
         print(f'El valor sera escrito como 0')
+        break
+while True:
     try:
         print(f"""
-              ===================
-              ¿Que tipo de salida es?
-              ===================
-              """)
+                ===================
+                ¿Que tipo de salida es?
+                ===================
+                """)
         for i, x in enumerate(tipoSalida):
             print(i + 1 ,x)
-        tipoDeSalida = int(input(f'¿El tipo de salida es: '))
-        indexTipoSalida = tipoSalida[tipoDeSalida - 1]
-        print(indexTipoSalida)
+            tipoDeSalida = int(input(f'¿El tipo de salida es: '))
+            indexTipoSalida = tipoSalida[tipoDeSalida - 1]
+            print(indexTipoSalida)
+        break
     except:
         print("Algo salio mal")
         sys.exit(1)
-    print(f"""Los tados agregados fueron
-    La fecha es: {fecha}
-    La descripcion es: {indexDescripcion}
-    El responsable es: {indexNombre}
-    El peso es: {peso}
-    El valor es: {valor}
-    La fuente es: {indexTipoSalida}
-    """)
+
+print(f"""Los tados agregados fueron
+La fecha es: {fecha}
+La descripcion es: {indexDescripcion}
+El responsable es: {indexNombre}
+El peso es: {peso}
+El valor es: {valor}
+La fuente es: {indexTipoSalida}
+""")
+while True:
     try:
         confirmar = int(input("""
         ¿Quieres confirmar esta informacion?
@@ -113,28 +140,16 @@ Cual es tu opcion: """))
         """))
         if confirmar not in range(1,3):
             print("Opcion no valida")
-        elif confirmar == 1:
+        if confirmar == 1:
             hoja.append([fecha, indexDescripcion, indexNombre, peso, valor, indexTipoSalida,])
             print("Datos listos")
-        else:
+            break
+        elif confirmar == 2:
             print("Datos no guardados")
-            
-            
+            break         
     except:
         print("Tipo de dato no valido")
         sys.exit(1)
-except:
-    print("Tipo de dato incorrecto")
-    sys.exit(1)
-
-
-
-      
-#entrada = int(input(f'A que horas llegaste? '))
-#salida = int(input(f'A que horas te fuiste? '))
-
-
 
 wb.save("empleados.xlsx")
 print("listo")
-
--- reto2.py
+++ reto2.py
@@ -1,11 +1,14 @@
 infoDescripcion = ["Aguacate hass","Aguacate papelito","Bold","Banano","Durazno","Limion Comun","Limon mandarino","Limon tahiti","Mandarina","Mango tomy","Maracuya","Naranja tangelo","Naranja valencia","Panela","Platano","Platano maduro","Guanabana","Almuerzo-Desayuno","Arriendo casa","Arriedo local","Arriendo parqueadero","BCS carro","Chevyplan","Entrada abastos","Gasolina","Gasto personal","Insumo","Lavado","Mario","Onces","Prestamo","Parqueadero carro","Parqueadero moto","Self security","Servicio de energia","Servicio de agua","Venta caja","Venta nequi","Venta daviplata"]
 
-for i,x in enumerate(infoDescripcion):
-    print(x,i + 1)
+x = str(input("Fecha: "))
+
+try:
+    f = int(x)
+    final = "/".join([x[i:i+2] for i in range(0,len(x),2)])
+    print(final[0:8])
+except:
+    print("Esto no es compatible")
 
 
-x = int(input("Pon algo: "))
 
-l = infoDescripcion[x]
 
-print(l)
