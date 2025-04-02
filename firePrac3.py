8adcd6be766c48535b66ec4e8b4d0cc8dd66c7cb
Colp <mononeitor300@gmail.com>
2025-04-01 23:04:05 -0500

    1.3.2
    -Mostrar datos
    -Contar un dict

---
+++ firePrac3.py
@@ -0,0 +1,37 @@
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+baseDeDatos = cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+baseDeDatos = firestore.client()
+
+datos = baseDeDatos.collection("Equipo").document("losTilines")
+
+doc = baseDeDatos.collection("Equipo").get()
+""" jugador = None
+for dato in doc:
+    datoJson = dato.to_dict()
+    for jugadores in datoJson.get('equipos', []):
+        if jugadores['posicion'] == "EnCuatro":
+            jugador = jugadores
+            print(jugador)
+            break
+
+
+datos.update({'equipos':firestore.ArrayRemove([jugador])})
+datos.update({'equipos':firestore.ArrayUnion([{'nombre': 'pedro','posicion':'delantero'}])}) """
+
+conteo = {}
+
+for diccio in doc:
+    docDict = diccio.to_dict()
+    for jugadores in docDict.get('equipos', []):
+        posicion = jugadores['posicion']
+        conteo[posicion] = conteo.get(posicion, 0) + 1
+
+print(conteo)
+
+print(f'defensas: {conteo["defensa"]}')
+print(f'porteros: {conteo["portero"]}')
+print(f'delanteros: {conteo["delantero"]}')
